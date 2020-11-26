3f7bace068f3d8bba9c2d94f78afb72660d186ca
cannarin025 <cannarin025@gmail.com>
2020-11-26 16:06:20 +0000

    analysis repo

---
+++ Analysis.py
@@ -0,0 +1,5 @@
+import scipy as sp
+import matplotlib.pyplot as plt
+
+dat = sp.loadtxt("C:\\Users\\canna\\Documents\\Imperial\\Physics\\3rd Year\\Lab\\Microprocessors\\Code\\Data\\Waveform_Data")
+print(dat)

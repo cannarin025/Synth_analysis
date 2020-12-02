819006bdd732da8ad1ca0d6103743c2ae2e7301d
Can Narin <cannarin025@gmail.com>
2020-12-02 18:21:29 +0000

    Added plots of other waveforms

---
+++ sin_plotter.py
@@ -0,0 +1,28 @@
+import scipy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+wavetype = "Sine"
+note = "C3"
+
+#unfiltered
+t_data, y_data = np.loadtxt(f"Data\\Waveform_Data\\{wavetype}\\Unfiltered\\{note}.CSV", delimiter=",", skiprows=1, unpack=True) #runs all files (comment out as you see fit)
+
+plt.figure()
+plt.plot(t_data, y_data)
+
+plt.xlabel("Time [s]")
+plt.ylabel("Voltage [V]")
+plt.grid()
+
+#filtered
+t_data, y_data = np.loadtxt(f"Data\\Waveform_Data\\{wavetype}\\Filtered\\{note}.CSV", delimiter=",", skiprows=1, unpack=True)
+
+plt.figure()
+plt.plot(t_data, y_data)
+
+plt.xlabel("Time [s]")
+plt.ylabel("Voltage [V]")
+plt.grid()
+
+plt.show()
